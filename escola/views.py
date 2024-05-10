589d7347291d4a3f9501f175addbf0374a0eded0
Ivan Wilhelm <contato@ivanwilhelm.dev>
2024-05-10 10:36:57 -0300

    feat: Adicionado uma camada de autenticação nas APIs.

--- escola/views.py
+++ escola/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets, generics
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, \
@@ -9,23 +11,31 @@ class AlunosViewSet(viewsets.ModelViewSet):
     """ Exibir todos os alunos e alunas """
     queryset = Aluno.objects.all()
     serializer_class = AlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class CursosViewSet(viewsets.ModelViewSet):
     """ Exibir todos os cursos """
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class MatriculaViewSet(viewsets.ModelViewSet):
     """ Exibir todas as matrículas """
     queryset = Matricula.objects.all()
     serializer_class = MatriculaSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class ListaMatriculasAlunoViewSet(generics.ListAPIView):
     """ Exibir todos as matrículas de um aluno """
     serializer_class = ListaMatriculasAlunoSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
@@ -35,6 +45,8 @@ class ListaMatriculasAlunoViewSet(generics.ListAPIView):
 class ListaAlunosMatriculadosViewSet(generics.ListAPIView):
     """ Exibir todos os alunos matriculados em um curso """
     serializer_class = ListaAlunosMatriculadosSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
